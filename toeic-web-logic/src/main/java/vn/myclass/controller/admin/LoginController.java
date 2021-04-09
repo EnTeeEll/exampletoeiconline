2536fb54080605b13c31cd185f6d74a242ee1364
EnTeeEll <Longcn9@gmail.com>
2021-04-09 16:47:10 +0700

    Create FormUitl.java to uses Beanutils

--- toeic-web-logic/src/main/java/vn/myclass/controller/admin/LoginController.java
+++ toeic-web-logic/src/main/java/vn/myclass/controller/admin/LoginController.java
@@ -1,5 +1,10 @@
 package vn.myclass.controller.admin;
 
+import org.apache.log4j.Logger;
+import vn.myclass.command.UserCommand;
+import vn.myclass.core.dto.UserDTO;
+import vn.myclass.core.web.Utils.FormUtil;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +15,9 @@ import java.io.IOException;
 
 @WebServlet("/login.html")
 public class LoginController extends HttpServlet {
+
+    private transient final Logger log = Logger.getLogger(this.getClass());
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
@@ -20,5 +28,11 @@ public class LoginController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+
+        UserCommand userCommand = FormUtil.populate(UserCommand.class,req);
+        UserDTO pojo = userCommand.getPojo();
+
+        RequestDispatcher rd = req.getRequestDispatcher("/views/web/login.jsp");
+        rd.forward(req,resp);
     }
 }
